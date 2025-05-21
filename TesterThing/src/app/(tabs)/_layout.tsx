de0760773b0d63599023a5b7f37ef018a42281bc
Bryan Wilson <goofymanbeek2.0@gmail.com>
2025-05-21 12:41:43 -0400

    Tab bar

--- TesterThing/src/app/(tabs)/_layout.tsx
+++ TesterThing/src/app/(tabs)/_layout.tsx
@@ -1,7 +1,42 @@
 import { Tabs } from "expo-router";
-
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 export default function TabsLayout() {
   return (
-    <Tabs />
+    <Tabs
+    screenOptions={{
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor: 'gray',
+      tabBarShowLabel: false,
+    }}
+    >
+      <Tabs.Screen name="index" options={{
+        title: 'Feed',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="home" color={color} size={size} />
+        ),
+      }} />
+      <Tabs.Screen name="notifications" options={{
+        title: 'Notifications',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="bell" color={color} size={size} />
+        ),
+        
+      }} />
+      <Tabs.Screen name="search" options={{
+        title: 'Search',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="magnify" color={color} size={size} />
+        ),
+      }} />,
+      <Tabs.Screen name="profile" options={{
+        title: 'Profile',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="account" color={color} size={size} />
+        ),
+      }} />
+
+      
+      
+    </Tabs>
   );
 }
