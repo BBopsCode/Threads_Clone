f8af5cf1dd7ab12ae0e897d1d47e318bf1b75557
Bryan Wilson <Bebop@Niveas-MacBook-Pro-2.local>
2025-06-02 14:49:21 -0400

    Types

---
+++ TesterThing/src/types.ts
@@ -0,0 +1,31 @@
+export type User = {
+    id: string;
+    username: string;
+    name: string;
+    image: string;
+}
+
+export type Workout = {
+    id: string;
+    name: string;
+    description: string;
+    goal: number;
+    completed_amount: number;
+
+    created_at: string;
+    updated_at: string;
+    
+    user_id: string;
+    user: User;
+
+    comments: Comment[];
+}
+
+export type Comment = {
+    id: string;
+    content: string;
+    created_at: string;
+    updated_at: string;
+    user_id: string;
+    user: User;
+}
